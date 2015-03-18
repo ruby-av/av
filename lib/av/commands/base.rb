90341a9b6fec376d79d8793c6aae1823880282b9
Sergey Margaritov <axeriano@gmail.com>
2015-03-18 14:38:52 +0200

    add width, height, duration to meta information

--- lib/av/commands/base.rb
+++ lib/av/commands/base.rb
@@ -15,18 +15,18 @@ module Av
 
       attr_accessor :source
       attr_accessor :destination
-      
+
       def initialize(options = {})
         reset_input_filters
         reset_output_filters
         reset_default_filters
         @options = options
       end
-      
+
       def add_source src
         @source = src
       end
-      
+
       def add_destination dest
         # infer format from extension unless format has already been set
         if @output_format.nil?
@@ -34,21 +34,21 @@ module Av
         end
         @destination = dest
       end
-      
+
       def reset_input_filters
         @input_params = ParamHash.new
         @audio_filters = ParamHash.new
         @video_filters = ParamHash.new
       end
-      
+
       def reset_output_filters
         @output_params = ParamHash.new
       end
-      
+
       def reset_default_filters
         @default_params = ParamHash.new
       end
-      
+
       def add_input_param *param
         p = parse_param(param)
         ::Av.log "Adding input parameter #{p}"
@@ -56,11 +56,11 @@ module Av
         @input_params[p[0]] << p[1]
         self
       end
-      
+
       def set_input_params hash
         @input_params = hash
       end
-      
+
       def add_output_param *param
         p = parse_param(param)
         ::Av.log "Adding output parameter #{p}"
@@ -68,11 +68,11 @@ module Av
         @output_params[p[0]] << p[1]
         self
       end
-      
+
       def set_output_params hash
         @output_params = hash
       end
-      
+
       def run
         raise Av::CommandError if (@source.nil? && @destination.nil?) || @command_name.nil?
 
@@ -90,7 +90,7 @@ module Av
         command_line = parameters.flatten.compact.join(" ").strip.squeeze(" ")
         ::Av.run(command_line)
       end
-      
+
       def identify path
         meta = {}
         command = %Q(#{@command_name} -i "#{File.expand_path(path)}" 2>&1)
@@ -104,7 +104,10 @@ module Av
           if line =~ /Video:(.*)/
             size = $1.to_s.match(/\d{3,5}x\d{3,5}/).to_s
             meta[:size] = size unless size.empty?
-            meta[:aspect] = size.split('x').first.to_f / size.split('x').last.to_f if meta[:size]
+            if meta[:size]
+              meta[:width], meta[:height] = meta[:size].split('x').map(&:to_i)
+              meta[:aspect] = meta[:width].to_f / meta[:height].to_f
+            end
           end
           # Matching Stream #0.0: Audio: libspeex, 8000 Hz, mono, s16
           if line =~ /Audio:(.*)/
@@ -113,19 +116,20 @@ module Av
           # Matching Duration: 00:01:31.66, start: 0.000000, bitrate: 10404 kb/s
           if line =~ /Duration:(\s.?(\d*):(\d*):(\d*\.\d*))/
             meta[:length] = $2.to_s + ":" + $3.to_s + ":" + $4.to_s
+            meta[:duration] = $2.to_i * 3600 + $3.to_i * 60 + $4.to_f
           end
           if line =~ /rotate\s*:\s(\d*)/
             meta[:rotate] = $1.to_i
           end
         end
         if meta.empty?
-          ::Av.log "Empty metadata from #{path}. Got the following output: #{out}" 
+          ::Av.log "Empty metadata from #{path}. Got the following output: #{out}"
         else
           return meta
         end
         nil
       end
-      
+
       def output_format format
         @output_format = format
         case format.to_s
@@ -145,12 +149,12 @@ module Av
           add_output_param 'strict', 'experimental'
         end
       end
-      
+
       # Children should override the following methods
       def filter_rotate degrees
         raise ::Av::FilterNotImplemented, 'rotate'
       end
-      
+
       def parse_param param
         list = []
         if param.count == 2
@@ -165,6 +169,6 @@ module Av
         end
         list
       end
-    end  
+    end
   end
 end
